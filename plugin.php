ded4961423d9d9e14b2975fd5e6a667b4249b002
ryanjames <ryan@ryanjam.es>
2021-04-13 12:22:08 -0700

    Initial commit

---
+++ plugin.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * 
+ * Remove Editor Formatting
+ * 
+ * @package   RemoveEditorFormatting
+ * @author    ryanjames
+ * @link      https://ryanjam.es
+ * @license   GPL-2.0-or-later
+ * 
+ * @wordpress-plugin
+ * Plugin Name: Remove Editor Formatting
+ * Plugin URI:  https://github.com/ryanjames/remove-editor-formatting
+ * Description: Removes default injected Gutenberg editor formatting
+ * Version:     1.0.0
+ * Author:      Ryan James
+ * Author URI:  https://ryanjam.es
+ * Text Domain: remove-editor-formatting 
+ * License:     GPL v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+ namespace RemoveEditorFormatting;
